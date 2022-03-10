11aa24809a7fc40b5e4d29d31ab9d07d60b80a4f
Robert Harvey <95311830+rjharvey10@users.noreply.github.com>
2022-03-09 18:21:40 -0800

    Add files via upload

---
+++ sql-challenge/SQL_HW_Output.sql
@@ -0,0 +1,137 @@
+-- Data Engineering --
+-- Drop Tables if Existing
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+-- Import CSV Files Into Corresponding SQL Table
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" INT   NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+	"emp_title_id" VARCHAR   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
+REFERENCES "employees" ("emp_title_id");
+
+-- Query * FROM Each Table Confirming Data
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+--Import each CSV file into the corresponding SQL table. Note be sure to import the data in the same 
+--order that the tables were created and account for the headers when importing to avoid errors.
+
+--1. List the following details of each employee: emp_no, last_name, first_name, sex, and salary.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries
+ON employees.emp_no = salaries.emp_no;
+
+--2. List first_name, last_name, and hire_date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date 
+FROM employees
+WHERE hire_date BETWEEN '1/1/1986' AND '12/31/1986';
+
+--3. List the manager of each department with the following information: dept_no, dept_name, the manager's emp_no, last_name, first_name.
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments
+JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
+
+--4. List the department of each employee with the following information: emp_no, last_name, first_name, and dept_name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no;
+
+--5. List first_name, last_name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT employees.first_name, employees.last_name, employees.sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name Like 'B%'
+
+--6. List all employees in the Sales department, including their emp_no, last_name, first_name, and dept_name.
+SELECT employees.emp_no, departments.dept_name, employees.last_name, employees.first_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
+
+--7. List all employees in the Sales and Development departments, including their emp_no, last_name, first_name, and dept_name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales' 
+OR departments.dept_name = 'Development';
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name,
+COUNT(last_name) AS "frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY
+COUNT(last_name) DESC;
